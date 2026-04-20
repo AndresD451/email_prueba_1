7c19cef0ad46423321a575942b8cc2ba4420ef38
Andrés Díaz <andresalbertodiazrevelo@gmail.com>
2026-04-20 15:11:51 -0600

    Clase Email

---
+++ Correo_Electronico_Q2_26/src/correo_electronico_q2_26/Email.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package correo_electronico_q2_26;
+
+/**
+ *
+ * @author user
+ */
+public class Email {
+    
+}
